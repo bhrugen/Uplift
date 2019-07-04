400e534015418bc003e6ae154faba9e235bfa48a
Ben Spark <bhrugen1990@gmail.com>
2019-07-04 16:19:55 -0500

    Section 5 - Assignment 2 - Add Frequency Repository and IRepository

---
+++ Uplift.DataAccess/Data/Repository/FrequencyRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
+
+namespace Uplift.DataAccess.Data.Repository
+{
+    public class FrequencyRepository : Repository<Frequency> , IFrequencyRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public FrequencyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<SelectListItem> GetFrequencyListForDropDown()
+        {
+            return _db.Frequency.Select(i => new SelectListItem()
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+        }
+
+        public void Update(Frequency frequency)
+        {
+            var objFromDb = _db.Frequency.FirstOrDefault(s => s.Id == frequency.Id);
+
+            objFromDb.Name = frequency.Name;
+            objFromDb.FrequencyCount = frequency.FrequencyCount;
+
+            _db.SaveChanges();
+        }
+
+    }
+}
---
+++ Uplift.DataAccess/Data/Repository/IRepository/IFrequencyRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Uplift.Models;
+
+namespace Uplift.DataAccess.Data.Repository.IRepository
+{
+    public interface IFrequencyRepository : IRepository<Frequency>
+    {
+        IEnumerable<SelectListItem> GetFrequencyListForDropDown();
+
+        void Update(Frequency frequency);
+    }
+}
