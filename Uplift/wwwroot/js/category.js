07997f1e496b8e80c751e77046be35f95c1bb9b0
Ben Spark <bhrugen1990@gmail.com>
2019-07-04 16:07:18 -0500

    Section 4 - Delete Category

--- Uplift/wwwroot/js/category.js
+++ Uplift/wwwroot/js/category.js
@@ -37,3 +37,34 @@ function loadDataTable() {
         "width":"100%"
     });
 }
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to delete?",
+        text: "You will not be able to restore the content!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        closeOnconfirm: true
+    }, function () {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (data) {
+                if (data.success) {
+                    toastr.success(data.message);
+                    dataTable.ajax.reload();
+                }
+                else {
+                    toastr.error(data.message);
+                }
+            }
+        });
+    });
+}
+
+function ShowMessage(msg) {
+    
+}
+
