d3cb253f9afe1727e86749202cb5ef8dbeb1d4b1
Ben Spark <bhrugen1990@gmail.com>
2019-07-04 11:27:01 -0500

    Section 3 - Implement ICategoryRepository

--- Uplift.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
+++ Uplift.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Uplift.Models;
 
 namespace Uplift.DataAccess.Data.Repository.IRepository
 {
-    interface ICategoryRepository
+    interface ICategoryRepository : IRepository<Category>
     {
+        IEnumerable<SelectListItem> GetCategoryListForDropDown();
+
+        void Update(Category category);
     }
 }
--- Uplift.Models/Category.cs
+++ Uplift.Models/Category.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Uplift.Models
 {
-    class Category
+    public class Category
     {
         [Key]
         public int Id { get; set; }
