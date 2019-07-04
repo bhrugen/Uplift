c894918045792b79de64316c3355886c61d60979
Ben Spark <bhrugen1990@gmail.com>
2019-07-04 16:26:55 -0500

    Section 5 - Assignment 4 - Add Frequency Controller and Index View

---
+++ Uplift/Areas/Admin/Controllers/FrequencyController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository.IRepository;
+
+namespace Uplift.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FrequencyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FrequencyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+
+        public IActionResult GetAll()
+        {
+
+            return Json(new { data = _unitOfWork.Frequency.GetAll() });
+        }
+
+        #endregion
+    }
+}
---
+++ Uplift/wwwroot/js/frequency.js
@@ -0,0 +1,41 @@
+﻿var  dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/admin/frequency/GetAll",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "25%" },
+            { "data": "frequencyCount", "width": "25%" },
+            {
+                "data": "id",
+                "render": function (data) {
+
+                    return `<div class="text-center"> 
+                            <a href="/Admin/frequency/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;' >
+                                <i class='far fa-edit'></i> Edit
+                            </a>
+                            &nbsp;
+                            <a class='btn btn-danger text-white' style='cursor:pointer; width:100px;' onclick=Delete('/admin/frequency/Delete/'+${data})>
+                               <i class='far fa-trash-alt'></i> Delete
+                            </a></div>
+                        `;
+                }, "width": "30%"
+            }
+
+
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+}
