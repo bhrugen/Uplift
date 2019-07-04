e91e72d18ab5e38bd9c0bbf1e0f0ba0d5c000e69
Ben Spark <bhrugen1990@gmail.com>
2019-07-04 15:47:46 -0500

    Section 4 - Upsert Post Action Method

--- Uplift/Areas/Admin/Controllers/CategoryController.cs
+++ Uplift/Areas/Admin/Controllers/CategoryController.cs
@@ -40,6 +40,26 @@ namespace Uplift.Areas.Admin.Controllers
 
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
 
         #region API CALLS
 
