d88555366d79a66b01e765ac510839adaeba475e
Ben Spark <bhrugen1990@gmail.com>
2019-07-04 11:39:05 -0500

    Section 3 - Implement CategoryRepository

--- Uplift.DataAccess/Data/ApplicationDbContext.cs
+++ Uplift.DataAccess/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Uplift.Models;
 
 namespace Uplift.DataAccess.Data
 {
@@ -12,5 +13,7 @@ namespace Uplift.DataAccess.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Category { get; set; }
     }
 }
--- Uplift.DataAccess/Data/Repository/CategoryRepository.cs
+++ Uplift.DataAccess/Data/Repository/CategoryRepository.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
 
 namespace Uplift.DataAccess.Data.Repository
 {
-    class CategoryRepository
+    public class CategoryRepository : Repository<Category> , ICategoryRepository
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<SelectListItem> GetCategoryListForDropDown()
+        {
+            return _db.Category.Select(i => new SelectListItem()
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Category.FirstOrDefault(s => s.Id == category.Id);
+
+            objFromDb.Name = category.Name;
+            objFromDb.DisplayOrder = category.DisplayOrder;
+
+            _db.SaveChanges();
+
+        }
     }
 }
