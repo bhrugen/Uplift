47b0dca9eb9a82ce84c54959b70871021db26952
Ben Spark <bhrugen1990@gmail.com>
2019-07-04 16:51:44 -0500

    Section 6 - Add Service.js

---
+++ Uplift/wwwroot/js/service.js
@@ -0,0 +1,72 @@
+﻿var  dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/admin/service/GetAll",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "20%" },
+            { "data": "category.name", "width": "20%" },
+            { "data": "price", "width": "15%" },
+            { "data": "frequency.FrequencyCount", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+
+                    return `<div class="text-center"> 
+                            <a href="/Admin/service/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;' >
+                                <i class='far fa-edit'></i> Edit
+                            </a>
+                            &nbsp;
+                            <a class='btn btn-danger text-white' style='cursor:pointer; width:100px;' onclick=Delete('/admin/service/Delete/'+${data})>
+                               <i class='far fa-trash-alt'></i> Delete
+                            </a></div>
+                        `;
+                }, "width": "30%"
+            }
+
+
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure want to Delete?",
+        text: "You will not be able to restore the file!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        closeOnConfirm: true
+    }, function () {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (data) {
+                if (data.success) {
+                    toastr.success(data.message);
+                    dataTable.ajax.reload();
+
+                }
+                else {
+                    toastr.error(data.message);
+                }
+            }
+        });
+    });
+
+
+}
