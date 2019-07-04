a8e938863ac711d3ea13ce7091dadf405f4f731a
Ben Spark <bhrugen1990@gmail.com>
2019-07-04 15:20:29 -0500

    Section 4 - Upsert Get Action Method

--- Uplift/Areas/Admin/Controllers/CategoryController.cs
+++ Uplift/Areas/Admin/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
 
 namespace Uplift.Areas.Admin.Controllers
 {
@@ -22,6 +23,22 @@ namespace Uplift.Areas.Admin.Controllers
             return View();
         }
 
+
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            if (id == null)
+            {
+                return View(category);
+            }
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+
+        }
 
 
         #region API CALLS
