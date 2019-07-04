a98369f9ba103b399551a185f4ea4ea646747a04
Ben Spark <bhrugen1990@gmail.com>
2019-07-04 10:48:10 -0500

    Section 3 - Create Category Model

---
+++ Uplift.Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Uplift.Models
+{
+    class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name="Category Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
