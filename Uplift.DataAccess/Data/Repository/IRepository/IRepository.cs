0e430588076578fa3455662817e0c122b4dedd77
Ben Spark <bhrugen1990@gmail.com>
2019-07-04 11:02:58 -0500

    Section 3 - Implement IRepository

--- Uplift.DataAccess/Data/Repository/IRepository/IRepository.cs
+++ Uplift.DataAccess/Data/Repository/IRepository/IRepository.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Uplift.DataAccess.Data.Repository.IRepository
 {
-    interface IRepository
+    public interface IRepository<T> where T : class
     {
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
+
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = null
+            );
+
+        void Add(T entity);
+
+        void Remove(int id);
+        void Remove(T entity);
     }
 }
