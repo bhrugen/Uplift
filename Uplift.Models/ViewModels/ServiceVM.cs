21695fde39beeec34513acc878e681f9a959a751
Ben Spark <bhrugen1990@gmail.com>
2019-07-04 17:03:37 -0500

    Section 6 - Service Index API and Service View Model

---
+++ Uplift.Models/ViewModels/ServiceVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uplift.Models.ViewModels
+{
+    public class ServiceVM
+    {
+        public Service Service {get;set;}
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+        public IEnumerable<SelectListItem> FrequencyList { get; set; }
+    }
+}
---
+++ Uplift/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository.IRepository;
+
+namespace Uplift.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public ServiceController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API Calls
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitOfWork.Service.GetAll(includeProperties: "Category,Frequency") });
+        }
+        #endregion
+    }
+}
