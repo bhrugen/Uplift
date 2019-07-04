b127a614af63f0e275cd10e682c3f8115ff66089
Ben Spark <bhrugen1990@gmail.com>
2019-07-04 15:16:47 -0500

    Section 4 - Category Datatable

--- Uplift/wwwroot/js/category.js
+++ Uplift/wwwroot/js/category.js
@@ -1,1 +1,39 @@
-﻿
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/admin/category/GetAll",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "50%" },
+            { "data": "displayOrder", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a href="/Admin/category/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;'>
+                                    <i class='far fa-edit'></i> Edit
+                                </a>
+                                &nbsp;
+                                <a onclick=Delete("/Admin/category/Delete/${data}") class='btn btn-danger text-white' style='cursor:pointer; width:100px;'>
+                                    <i class='far fa-trash-alt'></i> Delete
+                                </a>
+                            </div>
+                            `;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable":"No records found."
+        },
+        "width":"100%"
+    });
+}
