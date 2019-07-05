da330b4edb94fef0bf12ed019eae85bd0427ba7e
Ben Spark <bhrugen1990@gmail.com>
2019-07-04 19:21:34 -0500

    Section 7 - Home View Model

---
+++ Uplift.Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uplift.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Category> CategoryList { get; set; }
+        public IEnumerable<Service> ServiceList { get; set; }
+    }
+}
