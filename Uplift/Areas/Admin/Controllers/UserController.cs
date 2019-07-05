ddb24db32a8eaac0429516c97f517ea2c2a11015
Ben Spark <bhrugen1990@gmail.com>
2019-07-05 17:35:26 -0500

    Section 10 -Lock/Unlock User

--- Uplift/Areas/Admin/Controllers/UserController.cs
+++ Uplift/Areas/Admin/Controllers/UserController.cs
@@ -24,5 +24,25 @@ namespace Uplift.Areas.Admin.Controllers
 
             return View(_unitOfWork.User.GetAll(u=>u.Id!=claims.Value));
         }
+
+        public IActionResult Lock(string id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.User.LockUser(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult UnLock(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.User.UnLockUser(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
