77927dfa9c7fd5a6767d0d69f7ba551ead7adff9
Ben Spark <bhrugen1990@gmail.com>
2019-07-05 12:27:25 -0500

    Section 9 - Remove Item From Shopping Cart

--- Uplift/Areas/Customer/Controllers/CartController.cs
+++ Uplift/Areas/Customer/Controllers/CartController.cs
@@ -43,5 +43,16 @@ namespace Uplift.Areas.Customer.Controllers
             }
                 return View(CartVM);
         }
+
+
+        public IActionResult Remove(int serviceId)
+        {
+            List<int> sessionList = new List<int>();
+            sessionList = HttpContext.Session.GetObject<List<int>>(SD.SessionCart);
+            sessionList.Remove(serviceId);
+            HttpContext.Session.SetObject(SD.SessionCart, sessionList);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
