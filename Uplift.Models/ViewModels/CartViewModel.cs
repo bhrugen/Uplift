71bf96d637c9e31edc2eb43269b8ceab17dbff7b
Ben Spark <bhrugen1990@gmail.com>
2019-07-05 11:45:01 -0500

    Section 9 - Cart View Model

---
+++ Uplift.Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uplift.Models.ViewModels
+{
+    public class CartViewModel
+    {
+        public IList<Service> ServiceList { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
