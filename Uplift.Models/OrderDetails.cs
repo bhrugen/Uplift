b80a26613ba791f47fad334086ea2a1af4377ee3
Ben Spark <bhrugen1990@gmail.com>
2019-07-05 11:21:33 -0500

    Section 9 - Create Order Header and Details Model

---
+++ Uplift.Models/OrderDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Uplift.Models
+{
+    public class OrderDetails
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int OrderHeaderId { get; set; }
+
+        [ForeignKey("OrderHeaderId")]
+        public OrderHeader OrderHeader { get; set; }
+
+
+        [Required]
+        public int ServiceId { get; set; }
+
+        [ForeignKey("ServiceId")]
+        public Service Service { get; set; }
+
+        [Required]
+        public string ServiceName { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+    }
+}
---
+++ Uplift.Models/OrderHeader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Uplift.Models
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string ZipCode { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Status { get; set; }
+        public string Comments { get; set; }
+        public int ServiceCount { get; set; }
+
+    }
+}
