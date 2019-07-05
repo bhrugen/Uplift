ad4f511cbd1ab41a8f66a6a9fddf6407dfb2471e
Ben Spark <bhrugen1990@gmail.com>
2019-07-05 16:39:48 -0500

    Section 10 - Register User View

--- Uplift/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Uplift/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -59,6 +59,13 @@ namespace Uplift.Areas.Identity.Pages.Account
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
 
+            public string Name { get; set; }
+            public string StreetAddress { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string PostalCode { get; set; }
+
+            public string PhoneNumber { get; set; }
 
         }
 
