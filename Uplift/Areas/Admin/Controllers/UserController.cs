f4bdae71fb791604326e44faa14bb5d6450c99bb
Ben Spark <bhrugen1990@gmail.com>
2019-07-05 17:14:11 -0500

    Section 10 - Add User Controller

---
+++ Uplift/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository.IRepository;
+
+namespace Uplift.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            return View(_unitOfWork.User.GetAll(u=>u.Id!=claims.Value));
+        }
+    }
+}
