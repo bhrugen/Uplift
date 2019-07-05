88fef8aa3013b0de9f3aeacf275b824a0e849cdb
Ben Spark <bhrugen1990@gmail.com>
2019-07-04 20:59:12 -0500

    Section 8 - AddToCart Action

---
+++ Uplift.Utility/SD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uplift.Utility
+{
+    public static class SD
+    {
+        public const string SessionCart = "Cart";
+    }
+}
--- Uplift/Areas/Customer/Controllers/HomeController.cs
+++ Uplift/Areas/Customer/Controllers/HomeController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Extensions;
 using Uplift.Models;
 using Uplift.Models.ViewModels;
+using Uplift.Utility;
 
 namespace Uplift.Controllers
 {
@@ -36,6 +39,28 @@ namespace Uplift.Controllers
         {
             var serviceFromDb = _unitOfWork.Service.GetFirstOrDefault(includeProperties: "Category,Frequency", filter: c => c.Id == id);
             return View(serviceFromDb);
+        }
+
+
+        public IActionResult AddToCart(int serviceId)
+        {
+            List<int> sessionList = new List<int>();
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(SD.SessionCart)))
+            {
+                sessionList.Add(serviceId);
+                HttpContext.Session.SetObject(SD.SessionCart, sessionList);
+            }
+            else
+            {
+                sessionList = HttpContext.Session.GetObject<List<int>>(SD.SessionCart);
+                if (!sessionList.Contains(serviceId))
+                {
+                    sessionList.Add(serviceId);
+                    HttpContext.Session.SetObject(SD.SessionCart, sessionList);
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
         }
 
 
