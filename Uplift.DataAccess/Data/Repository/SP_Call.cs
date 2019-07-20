79bc3ef51cc7a7aa88d00adb2f713edc6c196688
Ben Spark <bhrugen1990@gmail.com>
2019-07-20 16:46:31 -0500

    Section 13 -  Stored Proc Repository Implementation

---
+++ Uplift.DataAccess/Data/Repository/SP_Call.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using Dapper;
+using Microsoft.EntityFrameworkCore;
+using Uplift.DataAccess.Data.Repository.IRepository;
+
+namespace Uplift.DataAccess.Data.Repository
+{
+    public class SP_Call : ISP_Call
+    {
+        private readonly ApplicationDbContext _db;
+        private static string ConnectionString = "";
+
+        public SP_Call(ApplicationDbContext db)
+        {
+            _db = db;
+            ConnectionString = db.Database.GetDbConnection().ConnectionString;
+        }
+
+        
+
+        public T ExecuteReturnScaler<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                return (T)Convert.ChangeType(sqlCon.ExecuteScalar<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure), typeof(T));
+            }
+        }
+
+        public void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                sqlCon.Execute(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        public IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                return sqlCon.Query<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
