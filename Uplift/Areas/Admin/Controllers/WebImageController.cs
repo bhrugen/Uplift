365a6b413533a6ce4477426bd5accea41bac1853
Ben Spark <bhrugen1990@gmail.com>
2019-07-21 17:36:00 -0500

    Section 14 - Create WebImage Controller

---
+++ Uplift/Areas/Admin/Controllers/WebImageController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
+using Uplift.Utility;
+
+namespace Uplift.Areas.Admin.Controllers
+{
+    [Authorize]
+    [Area("Admin")]
+    public class WebImageController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public WebImageController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //public IActionResult Upsert(int? id)
+        //{
+        //    Category category = new Category();
+        //    if (id == null)
+        //    {
+        //        return View(category);
+        //    }
+        //    category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+        //    if (category == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(category);
+
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Upsert(Category category)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        if (category.Id == 0)
+        //        {
+        //            _unitOfWork.Category.Add(category);
+        //        }
+        //        else
+        //        {
+        //            _unitOfWork.Category.Update(category);
+        //        }
+        //        _unitOfWork.Save();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(category);
+        //}
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            
+            return Json(new { data = _db.WebImages.ToList() });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _db.Category.Find(id);
+            if(objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting." });
+            }
+
+            _db.Category.Remove(objFromDb);
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Delete successful." });
+
+        }
+
+
+        #endregion
+    }
+}
